011d9cd76dbb65b38791bd9cdddc5bbde15ad567
RoberthLoorDev <dayvidlife@gmail.com>
2024-07-03 22:41:18 -0500

    add routing and connection with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./features/auth/pages/LoginPage";
+import SignUpPage from "./features/auth/pages/SignUpPage";
+import { Toaster } from "sonner";
+
 function App() {
     return (
-        <>
-            <h1 className="bg-red-200">Hello world</h1>
-        </>
+        <div>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} Route />
+                <Route path="/sign-up" element={<SignUpPage />} Route />
+            </Routes>
+            <Toaster
+                toastOptions={{
+                    style: {
+                        background: "#D3D3D3",
+                    },
+                }}
+            />
+        </div>
     );
 }
 
